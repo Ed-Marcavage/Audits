ca305dfe26ed9186a9f925469450f230e36fc61c
Ed-Marcavage <edmarcavage256@proton.me>
2024-03-29 23:40:12 -0400

    chore: forge init

--- 3-passwordstore-audit/src/PasswordStore.sol
+++ 3-passwordstore-audit/src/PasswordStore.sol
@@ -4,13 +4,14 @@ pragma solidity 0.8.18;
 /*
  * @author not-so-secure-dev
  * @title PasswordStore
- * @notice This contract allows you to store a private password that others won't be able to see. 
+ * @notice This contract allows you to store a private password that others won't be able to see.
  * You can update your password at any time.
  */
 contract PasswordStore {
     error PasswordStore__NotOwner();
 
     address private s_owner;
+    //@audit - private variables are not actually private, they can be viewed by anyone
     string private s_password;
 
     event SetNetPassword();
@@ -23,6 +24,7 @@ contract PasswordStore {
      * @notice This function allows only the owner to set a new password.
      * @param newPassword The new password to set.
      */
+    //@audit - the setPassword function allows anyone to set the password, not just the owner
     function setPassword(string memory newPassword) external {
         s_password = newPassword;
         emit SetNetPassword();
@@ -32,6 +34,7 @@ contract PasswordStore {
      * @notice This allows only the owner to retrieve the password.
      * @param newPassword The new password to set.
      */
+    //@audit - there is no newPassword parameter in the getPassword function
     function getPassword() external view returns (string memory) {
         if (msg.sender != s_owner) {
             revert PasswordStore__NotOwner();
